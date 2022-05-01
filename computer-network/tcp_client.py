4012c87549766a6d6fc143ff09aeb2292b60d408
heewon916 <kimhiwon@naver.com>
2022-05-02 01:03:12 +0900

    succeed connected until client send exit

--- computer-network/tcp_client.py
+++ computer-network/tcp_client.py
@@ -2,13 +2,25 @@
 
 from socket import *
 
-serverName = '127.0.0.1'
-serverPort = 8080
+host = '127.0.0.1'
+serverPort = 9999
 clientSocket = socket(AF_INET, SOCK_STREAM)     # SOCK_STREAM: TCP 프로토콜을 사용한다는 의미
-clientSocket.connect((serverName, serverPort))
+clientSocket.connect((host, serverPort))
+print("Program for Lower to Upper and otherwise")
+case = str(input("[!] What case do you need?\n\t(1)Lower to Upper (2)Upper to Lower (1/2)\n\t>>"))
+clientSocket.send(case.encode())
 
-sentence = str(input('Input LowerCase sentence: '))
-clientSocket.send(sentence.encode())
-modifiedSentence = clientSocket.recv(1024)
-print("From server: ", modifiedSentence.decode())
+while True:    
+    sentence = str(input('Input :: '))
+    if not sentence:
+        print("(Client)Failed to write data\n")
+        break
+    if sentence == "exit":
+        print("(Client)bye\n")
+        break
+    clientSocket.send(sentence.encode())
+    print("(Client)send data ::", sentence)
+    modifiedSentence = clientSocket.recv(1024)
+    print("(Client)recieve data :: ", modifiedSentence.decode())
+    
 clientSocket.close()
--- computer-network/tcp_server.py
+++ computer-network/tcp_server.py
@@ -2,16 +2,40 @@
 
 from socket import *
 
-serverPort = 8080
+host = '127.0.0.1'
+serverPort = 9999
 serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.bind(('', serverPort))
+serverSocket.bind((host, serverPort))
 serverSocket.listen(1)      # 1명만 상대한다는 의미
+connectionSocket, addr = serverSocket.accept()  # 클라이언트가 전송한 내용을 받음
+print('Connected by', addr)
+case = connectionSocket.recv(4).decode()
+connectionSocket.send("(Server)start writing words")
 
-print('the server is ready to receive')
+if case == "1": #Lower to Upper
+    while True:
+        data = connectionSocket.recv(1024).decode()
+        if not data:
+            break
+        capitalize_data = data.upper()
+        connectionSocket.send(capitalize_data.encode())
+    
+elif case == "2": #Upper to Lower
+    while True:
+        data = connectionSocket.recv(1024).decode()
+        if not data:
+            break
+        capitalize_data = data.lower()
+        connectionSocket.send(capitalize_data.encode())
 
-while True:
-    connectionSocket, addr = serverSocket.accept()  # 클라이언트가 전송한 내용을 받음
-    sentence = connectionSocket.recv(1024).decode()
-    capitalizedSentence = sentence.upper()
-    connectionSocket.send(capitalizedSentence.encode())
-    #connectionSocket.close()
+else:
+    while True:
+        data = connectionSocket.recv(1024).decode()
+        if not data:
+            break
+        sentence = "case is wrong..."
+        connectionSocket.send(sentence.encode())
+        connectionSocket.close()
+        serverSocket.close()
+connectionSocket.close()
+serverSocket.close()
