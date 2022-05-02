c32f59a3e5271c25e3eb572a6b02963e1c1bbf9d
heewon916 <kimhiwon@naver.com>
2022-05-03 02:31:14 +0900

    error in login program: when the server send format error sent. client has no code for rcv

--- computer-network/tcp_client_login.py
+++ computer-network/tcp_client_login.py
@@ -2,16 +2,25 @@
 
 from socket import *
 
-serverName = '127.0.0.1'
+host = '127.0.0.1'
 serverPort = 8080
 clientSocket = socket(AF_INET, SOCK_STREAM)     # SOCK_STREAM: TCP 프로토콜을 사용한다는 의미
-clientSocket.connect((serverName, serverPort))
+clientSocket.connect((host, serverPort))
 
-print("***Start Login***")
-id = str(input('Input ID: '))
-pw = str(input('Input PW: '))
+print("Program for Login...")
+print("Usage: to quit >> write 'exit'")
+print("***Start Login***\n")
+while True:
+    id = str(input('Input ID: '))
+    pw = str(input('Input PW: '))
 
-clientSocket.send((id+' ' +pw).encode())
-modifiedSentence = clientSocket.recv(1024)
-print("From server: ", modifiedSentence.decode())
+    clientSocket.send((id+' ' +pw).encode())
+    modifiedSentence = clientSocket.recv(1024)
+    print("From server: ", modifiedSentence.decode())    
+    
+    keep_on_state = str(input("\tKeep on State?(y/n) >>"))
+    if keep_on_state == 'n':
+        clientSocket.send("exit".encode())
+        break
+print("(Client)Bye\n")
 clientSocket.close()
--- computer-network/tcp_server_login.py
+++ computer-network/tcp_server_login.py
@@ -7,28 +7,33 @@ user_list = {"Bob":"bob12!",
              "Hamong":"ham92!",
              "Nodes":"node63!@"}
 
+host = '127.0.0.1'
 serverPort = 8080
 serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.bind(('', serverPort))
-
-print('the server is ready to receive')
+serverSocket.bind((host, serverPort))
+serverSocket.listen(1)      # 1명만 상대한다는 의미
+connectionSocket, addr = serverSocket.accept()  # 클라이언트가 전송한 내용을 받음
+print('Connected by', addr)
+print('\nthe server is ready to receive\n')
 
 while True:
-    serverSocket.listen(1)      # 1명만 상대한다는 의미
     connectionSocket, addr = serverSocket.accept()  # 클라이언트가 전송한 내용을 받음
     guest_info = connectionSocket.recv(1024).decode()
     id, pw = guest_info.split(' ')
     # 아이디가 이미 존재하는 경우
-    if id in user_list:
-        sentence = "Your Info already exists"
+    if guest_info == "exit":
+        print("(Server)Bye\n")
         break
+    if id in user_list:
+        sentence = "[!] You ALREADY joined\n"        
+        connectionSocket.send(sentence.encode())
     # 아이디가 없는 경우: 회원가입을 진행 
     else:
-        if pw.isalnum() or len(pw) <= 4:
-            sentence = "[!] FORMAT ERROR: 비밀번호는 특수문자를 포함하여 4자리 이상이어야 합니다."
+        if pw.isalnum() or len(pw) <= 4 or pw.isalpha():
+            sentence = "[!] FORMAT ERROR: 비밀번호는 영문자와 특수문자를 포함하여 4자리 이상이어야 합니다."
             continue
         else:
             user_list[id] = pw 
             sentence = f"Welcome! User {id}"
-    connectionSocket.send(sentence.encode())
+        connectionSocket.send(sentence.encode())    
 connectionSocket.close()
